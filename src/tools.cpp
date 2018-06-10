ae2b0d7607f2ae28de9b2a0ff02ebf14f9ebb21c
Qianli <qianli.ma622@gmail.com>
2018-06-10 15:49:59 -0400

    finished prototyping

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "tools.h"
 
-using Eigen::VectorXd;
+using Eigen::Vector4d;
 using Eigen::MatrixXd;
 using std::vector;
 
@@ -15,4 +15,27 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+    Vector4d vecRMSE(0, 0, 0, 0);
+    if (estimations.size() != ground_truth.size() || estimations.size() < 1 )
+    {
+        return vecRMSE;
+    }
+
+    int n1 = estimations.size();
+
+    Vector4d residual;
+
+    for(int i = 0; i < n1; ++i)
+    {
+        residual = estimations.at(i) - ground_truth.at(i);
+        residual = residual.array()*residual.array();
+        vecRMSE += residual;
+    }
+
+    vecRMSE /= n1;
+    vecRMSE = vecRMSE.array().sqrt();
+
+    std::cout << "Final residual is " << vecRMSE << std::endl;
+
+    return vecRMSE;
 }
